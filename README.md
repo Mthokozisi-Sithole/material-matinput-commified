a6ddd72566b7bd8ebf9dd5b3169544502058f650
Mthokozisi-Sithole <mthokozisiephraim@gmail.com>
2022-05-10 18:21:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# material-matinput-commified-ja3isx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/material-matinput-commified-ja3isx)
---
+++ app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent  {
+  title = 'ngx-material-tools-demo';
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  myForm = this.formBuilder.group({
+    deposit: ['', [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(1000000)
+    ]
+    ],
+  });
+
+  onSubmit() {
+    console.log('form ', this.myForm, this.myForm.get('deposit'));
+  }
+
+}
